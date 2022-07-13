eb8c57500b4d35fdc56f882c2bc5e9783dc01f7a
kshit0708e <chauhanparikshit7@gmail.comm>
2022-07-13 18:06:41 +0530

    update 1

--- array.js
+++ array.js
@@ -34,16 +34,23 @@
 // let elements = ['Bike', 'Scooter', 'Car', 'Truck', 'JCB' , 'Helicopter'];
 // console.log(elements[4])
 
-function isDuplicate(arr){
-    let str=[];
-      for(let i=0;i<arr.length;i++){
-        let element=arr[i];
-        if(str.indexOf(element)!==-1){
-          return true;
-        }
-        str.push(element);
-      }
-      return false
-    }
-    console.log(isDuplicate([1, 2, 3, 3, 5]))
-    exports.isDuplicate = isDuplicate
+// function isDuplicate(arr){
+//     let str=[];
+//       for(let i=0;i<arr.length;i++){
+//         let element=arr[i];
+//         if(str.indexOf(element)!==-1){
+//           return true;
+//         }
+//         str.push(element);
+//       }
+//       return false
+//     }
+//     console.log(isDuplicate([1, 2, 3, 3, 5]))
+//     exports.isDuplicate = isDuplicate
+
+function goodManager(arr){
+    let newArr=arr.map(value=>value+1000);
+    return newArr
+  }
+  console.log(goodManager([1000,2000, 3000]))
+  exports.goodManager = goodManager
